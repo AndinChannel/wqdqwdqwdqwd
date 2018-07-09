be869f595e230d749be9bca3d60fe1c97cbc876a
AndinChannel <35931355+AndinChannel@users.noreply.github.com>
2018-07-09 22:53:33 +0700

    Update mizukimusic.js

--- mizukimusic.js
+++ mizukimusic.js
@@ -137,8 +137,7 @@ ${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}
         })
                         selection.delete();
                     }
-                    user = "";
-                    const videoIndex = parseInt(response.first().content && user.id === message.author.id);
+                    const videoIndex = parseInt(response.first().content);
                     var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
                 } catch (err) {
                     console.error(err)
